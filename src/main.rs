e1cb93e8d01a304e0fba0a6e144a64b968e0d277
Guangzong Chen <chen@ggeta.com>
2024-05-01 23:14:31 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,38 @@ use u5_lib::{
     i2c, i2c::I2c,
 };
 
-fn i2c_init() -> I2c {
-    let i2c_config = i2c::I2cConfig::new(1, 100_000, gpio::I2C1_SCL_PB6, gpio::I2C1_SDA_PB7);
-    I2c::new(i2c_config).unwrap()
+fn i2c_init() -> (I2c, I2c) {
+    let i2c_config_plus = i2c::I2cConfig::new(1, 100_000, gpio::I2C1_SCL_PB6, gpio::I2C1_SDA_PB3);
+    let i2c_plus = I2c::new(i2c_config_plus).unwrap();
+    let i2c_config_minus = i2c::I2cConfig::new(2, 100_000, gpio::I2C2_SCL_PB13, gpio::I2C2_SDA_PB14);
+    let i2c_minus = I2c::new(i2c_config_minus).unwrap();
+    (i2c_plus, i2c_minus)
 }
 
 #[task]
 async fn async_main(spawner: Spawner) {
-    // becareful, if the dbg is not enabled, but using deep sleep. This framework will not able to connect to chip.
+    // be careful, if the dbg is not enabled, but using deep sleep. This framework will not able to connect to chip.
     // stm32cube programmer, stmcubeide can be used to program the chip, then this framework can be used to debug.
     clock::init_clock(false, true, 16_000_000, true, clock::ClockFreqs::KernelFreq4Mhz);
     no_deep_sleep_request();
     defmt::info!("setup led finished!");
-    let green: gpio::GpioPort = gpio::PB7;
+    let red: gpio::GpioPort = gpio::PB7;
+    let green: gpio::GpioPort = gpio::PB8;
     green.setup();
-    green.set_high();
-    let i2c = i2c_init();
-    panic!("panic");
+    red.setup();
+    let (mut i2c_plus, mut i2c_minus) = i2c_init();
+    // let i2c_message = i2c::I2cMessage {
+    //     addr: 0x68,
+    //     data: &mut [0x75],
+    // };
+    // i2c_plus.send(&i2c_message).unwrap();
+    defmt::info!("i2c init finished!");
     loop {
-        exti::EXTI13_PC13.wait_for_raising().await;
+        // exti::EXTI13_PC13.wait_for_raising().await;
         green.toggle();
-        defmt::info!("toggle");
+        red.toggle();
+        delay_ms(500);
+        defmt::info!("toggle leds");
     }
 }
 
